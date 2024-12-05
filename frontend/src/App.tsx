6de39780fa7b141908c52b414ef153b4fd277d4a
dxmv <dimitirjestepanovic1@gmail.com>
2024-12-05 18:07:12 +0100

    Generated react frontend with typescipt & tailwind.css

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-3xl font-bold underline">Hello World</p>
+    </div>
+  );
+}
+
+export default App;
